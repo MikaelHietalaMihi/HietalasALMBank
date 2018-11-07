a31f75a894de26ceb14a58459b48ced67ffc26ed
Mikael Hietala <Mikael.Hietala@yh.nackademin.se>
2018-11-07 22:00:16 +0100

    Added Deposit and Withdraw methods to BankRepository

--- HietalasALMBank/Repositories/BankRepository.cs
+++ HietalasALMBank/Repositories/BankRepository.cs
@@ -7,5 +7,35 @@ namespace HietalasALMBank.Repositories
 {
     public class BankRepository
     {
+        public double Deposit(double balance, double amount)
+        {
+            return balance += amount;
+        }
+
+        public double Withdraw(double balance, double amount)
+        {
+            return balance -= amount;
+        }
+
+        public bool CheckIfWithdrawIsOk(double balance, double amount)
+        {
+            if (balance >= amount && amount > 0) {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckIfDepositIsOk(double balance, double amount)
+        {
+            if (amount > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+
     }
 }
