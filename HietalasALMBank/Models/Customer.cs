d0b3a84cb287293147ce8cf199948869ea0d66ee
Mikael Hietala <Mikael.Hietala@yh.nackademin.se>
2018-11-07 18:41:42 +0100

    Created Customer class

---
+++ HietalasALMBank/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HietalasALMBank.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname  { get; set; }
+    }
+}
