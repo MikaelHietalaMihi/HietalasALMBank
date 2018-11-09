4d3a8aca4b49e21faad27b4e15d5924e33aa5184
thomas lotsander <thomaslotsander@gmail.com>
2018-11-09 16:17:28 +0100

    Added validation on model

--- HietalasALMBank/Models/TransferViewModel.cs
+++ HietalasALMBank/Models/TransferViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,12 @@ namespace HietalasALMBank.Models
 {
     public class TransferViewModel
     {
-        public string fromAccountNumber { get; set; }
-        public string toAccountNumber { get; set; }
+
+        [Required(ErrorMessage = "Ett kontonummer är obligatoriskt")]
+        public string FromAccountNumber { get; set; }
+        [Required(ErrorMessage = "Ett kontonummer är obligatoriskt")]
+        public string ToAccountNumber { get; set; }
+        [Range(1, double.MaxValue)]
         public double Amount { get; set; }
 
     }
